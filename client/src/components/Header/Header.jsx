6aa4fad400736b79e9d701877b62519ffdc0b1c5
alundin2024 <lundin09@gmail.com>
2025-02-27 12:52:08 +0100

    Changed placeholder for search.

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -14,7 +14,7 @@ const Header = () => {
           <div className="flex items-center space-x-4">
             <input
               type="text"
-              placeholder="Search..."
+              placeholder="Sök efter produkt..."
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 md:w-80"
             />
             <div className="flex space-x-4 md:hidden">
