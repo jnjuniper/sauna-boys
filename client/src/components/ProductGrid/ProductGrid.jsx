0bfe01211aa11a279c20f0388d10ce5fc7212640
alundin2024 <lundin09@gmail.com>
2025-02-27 12:15:11 +0100

    Changed from dollar to SEK.

--- client/src/components/ProductGrid/ProductGrid.jsx
+++ client/src/components/ProductGrid/ProductGrid.jsx
@@ -21,7 +21,7 @@ const ProductGrid = () => {
           />
           <h2 className="text-lg font-semibold">{product.productName}</h2>
           <p className="text-sm text-gray-600">{product.brand}</p>
-          <p className="text-lg font-bold">${product.price}</p>
+          <p className="text-lg font-bold">{product.price} SEK :-</p>
         </div>
       ))}
     </div>
