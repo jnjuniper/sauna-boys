134ddbabb7a023f1d40e02addb6f04d42cf6115e
alundin2024 <lundin09@gmail.com>
2025-02-27 11:45:24 +0100

    Added product-grid for startpage.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 
 import './App.css'
-import Header from './pages/Header/Header.jsx'
+import Header from './components/Header/Header.jsx'
+import ProductGrid from './components/ProductGrid/ProductGrid.jsx'
 import { BrowserRouter, Routes, Route } from 'react-router'
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
     <Header />
     <main>
+      <ProductGrid />
       <Routes>
         <Route />
       </Routes>
