cbc2225b0c9d7a10ca7dcfa9d88b414b3ee76b30
Anton Lundin <lundin09@gmail.com>
2025-03-04 17:25:04 +0100

    Added admin-page for adding new products.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,30 +6,43 @@ import SearchPage from "./pages/SearchPage/SearchPage.jsx";
 import Hero from "./components/Hero/Hero.jsx";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router";
 import ResponsiveFooter from "./components/Footer/ResponsiveFooter";
+import NewProduct from "./pages/Admin/AdminAdd.jsx";
 
 function App() {
   const location = useLocation();
 
+  // Check if the current path is "/admin"
+  const isAdminPage = location.pathname.startsWith("/admin");
+
   return (
     <>
-      <Header
-        onSearch={(searchTerm) =>
-          (window.location.href = `/search?query=${searchTerm}`)
-        }
-      />
+      {/* Render Header only if it's NOT an admin page */}
+      {!isAdminPage && (
+        <Header
+          onSearch={(searchTerm) =>
+            (window.location.href = `/search?query=${searchTerm}`)
+          }
+        />
+      )}
 
       {/* Render Hero only on the homepage */}
-      {location.pathname === "/" && <Hero />}
+      {!isAdminPage && location.pathname === "/" && <Hero />}
 
       <main>
         <Routes>
           <Route path="/search" element={<SearchPage />} />
           <Route path="/" element={<ProductGrid />} />
+          <Route path="/admin/products/new" element={<NewProduct />} />
         </Routes>
       </main>
 
-      <InfoGrid />
-      <ResponsiveFooter />
+      {/* Render Footer only if it's NOT an admin page */}
+      {!isAdminPage && (
+        <>
+          <InfoGrid />
+          <ResponsiveFooter />
+        </>
+      )}
     </>
   );
 }
