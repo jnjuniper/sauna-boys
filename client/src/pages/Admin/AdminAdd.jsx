d3d39f9484ee3882488d85c6059369464aa39a97
Anton Lundin <lundin09@gmail.com>
2025-03-07 02:21:43 +0100

    Moved add-button on AdminAdd.

--- client/src/pages/Admin/AdminAdd.jsx
+++ client/src/pages/Admin/AdminAdd.jsx
@@ -169,7 +169,7 @@ function NewProduct() {
               />
             </div>
 
-            <div className="flex justify-end">
+            <div className="flex justify-start pt-4">
               <button
                 type="submit"
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
